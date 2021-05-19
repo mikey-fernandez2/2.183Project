ade8fe20ea4963d270d4a09071d587a462923906
guveayse <79320924+guveayse@users.noreply.github.com>
2021-05-18 20:53:34 -0400

    Calculated theta from direction instead of angle in csv file

--- extractTrajectory.m
+++ extractTrajectory.m
@@ -4,13 +4,34 @@ function extractTrajectory(fileName)
 
 % fileName = "MoCap.csv";
 
-raw = readtable(fileName);
+%raw = readtable(fileName);
+% shoulderAng = raw.shoulder_angle;
+% elbowAng = raw.elbow_angle;
+% wristAng = raw.wrist_angle;
+
+% loading the data
+data  = load(fileName); % ex: 'Direction2896.mat'
+data = data.Direction2896;
 frameRate = 60; % FPS
 frameTime = 1/frameRate; % s
 
-shoulderAng = raw.shoulder_angle;
-elbowAng = raw.elbow_angle;
-wristAng = raw.wrist_angle;
+% establishing frames
+frame1 = table2array(data);
+frame1(end,:) = [];
+frame2 = table2array(data);
+frame2(1,:) = [];
+
+%calculating theta
+multiplyframes12 = frame1.*frame2;
+dotproductframes12 = [multiplyframes12(:,1)+multiplyframes12(:,2) ...
+                     multiplyframes12(:,3)+multiplyframes12(:,4) ...
+                     multiplyframes12(:,5)+multiplyframes12(:,6)];
+
+theta12 = acos(dotproductframes12); % distance in radians from frame 1 to 2
+
+shoulderAng = theta12(:,1);
+elbowAng = theta12(:,2);
+wristAng = theta12(:,3);
 
 shoulderVel = diff(shoulderAng)/frameTime; shoulderVel = [shoulderVel; shoulderVel(end)];
 elbowVel = diff(elbowAng)/frameTime; elbowVel = [elbowVel; elbowVel(end)];
@@ -27,4 +48,3 @@ acc = [shoulderAcc'; elbowAcc'; wristAcc'];
 save('ComputedTorqueTrajectory.mat', 'pos', 'vel', 'acc')
 
 end
-
