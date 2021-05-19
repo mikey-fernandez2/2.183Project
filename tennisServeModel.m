7313c4b1c89a4b4fd50816c176a0034046c6bb5e
lauraachicos <40573212+lauraachicos@users.noreply.github.com>
2021-05-19 12:31:00 -0400

    Update tennisServeModel.m
    added plots

--- tennisServeModel.m
+++ tennisServeModel.m
@@ -23,8 +23,8 @@ r1 = 0.484*l1; r2 = 0.439*l2; r3 = 0.506*l3;    % m, distance to center of mass 
 rg1 = 0.322*l1; rg2 = 0.303*l2; rg3 = 0.297*l3; % m, radius of gyration of each link 
 Ic1 = m1*rg1^2; Ic2 = m2*rg2^2; Ic3 = m3*rg3^2; % kg-m^2, moment of inertia about mass center
 g = 9.81;                                       % m/s^2, gravity constant
-k1 = 0; k2 = 0; k3 = 0;                         % N-m/rad, joint stiffness constants
-b1 = 0; b2 = 0; b3 = 0;                         % N-m-s/rad, joint damping constants
+k1 = 10; k2 = 10; k3 = 10;                         % N-m/rad, joint stiffness constants
+b1 = .1; b2 = 0.1; b3 = 0.1;                         % N-m-s/rad, joint damping constants
 l_max = l1 + l2 + l3;                           % m, maximum arm reach
 
 p = [m1 m2 m3 Ic1 Ic2 Ic3 l1 l2 l3 r1 r2 r3 g k1 k2 k3 b1 b2 b3]';
@@ -40,7 +40,7 @@ om0 = [0; 0; 0];
 z0 = [th0; om0];
 
 if ~CONTROLLER
-    freq = 100;
+    freq = 60;
     tSpan = 0:1/freq:timeVec(end);
 else
     freq = 60;
@@ -66,6 +66,24 @@ for i = 1:length(tOut)
     
     pause(0.001)
 end
+
+% Plot Joint Trajectories
+figure
+plot(timeVec, th1, 'b')
+hold on
+plot(timeVec, th2, 'r')
+plot(timeVec, th3, 'g')
+title("Joint Trajectories")
+
+% Plot Hand Trajectory
+figure
+plot(timeVec, th3)
+title("Hand Trajectory");
+
+% Plot Hand Velocity
+figure
+plot(timeVec,om3)
+title("Hand Velocity");
 
 %% Functions
 function dxdt = stateEqs(t, z)
